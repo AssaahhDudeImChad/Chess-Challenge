f8cfbbc70d8eb047fffbe4bbfd11e024db439f35
AssaahhDudeImChad <seb@thelazybasset.com>
2023-07-28 09:13:13 +1200

    Went back to old version

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,7 +1,7 @@
 ﻿using ChessChallenge.API;
 using System.Collections;
 using System;
-
+using System.Linq;
 
 
 public class MyBot : IChessBot
@@ -9,28 +9,65 @@ public class MyBot : IChessBot
  
     //null, pawn, knight, bishop, rook, queen, king
     public Move Think(Board board, Timer timer){   
-        int score = 0;
-        int[] values = {0, 30, 50, 70, 60, 90, 100};
+        
         bool color = board.IsWhiteToMove;
-        
-        static int GetScore(Board board, int score, int[] values, bool color){
-            if(color == true){
-                var opponant_pieces = board.BlackPiecesBitboard;
-                Console.WriteLine("black pieces: "+ opponant_pieces);
-            }else{
-                var opponant_pieces = board.WhitePiecesBitboard;
-                Console.WriteLine("White peices: "+ opponant_pieces);
-            }
-            //get bitboard of the whole board, when there is a piece there use getpeice to get infro about that pieve
-            return 0;
-        }
-        GetScore(board, score, values, color);
-         
-        System.Random rand = new System.Random();
         Move[] moves = board.GetLegalMoves();
+        int[] scores = new int[moves.Length];
+        int[] values = {30, 50, 70, 60, 90, 100};
+
+        int move_weight(Board board, int[] values, Move move){
+            int count = 0;
+            PieceList[] pieces = board.GetAllPieceLists();
+            int countup(int[] values){
+                for(int i=0; i<6; i++){
+                    count +=(values[i]*pieces[i].Count);     
+                }
+                for(int x=0; x<6; x++){
+                    count -=(values[x]*pieces[x+6].Count);
+                    
+                }
+                return count;
+                
+            }
+            
+            int before = countup(values);
+            board.MakeMove(move);
+            int after = countup(values);
+            board.UndoMove(move);
+            //the weight modifier from captures
+            int capture_weight = (before-after);
+            //getting the weight modifier from checks
+
+            board.MakeMove(move);
+            bool ischeck = board.IsInCheck();
+            bool ismate = board.IsInCheckmate();
+            board.UndoMove(move);
+            int check_weight = ((Convert.ToInt32(ischeck)+Convert.ToInt32(ismate))*100);
+            Console.WriteLine((check_weight+capture_weight));
+            return(check_weight+capture_weight);
+            
+        }
+        int GetHighestScore(Board board, Move[] moves, int[] scores, int[] values){
+           
+            for(int i = 0; i++ < moves.Length-1;){
+                scores[i] += move_weight(board, values, moves[i]);
+            }
+            int highest_score = scores.Max();
+            int highest_index = Array.IndexOf(scores, highest_score);
+            return(highest_index);
+        }
         
-        int N = rand.Next(moves.Length);
-        return moves[N];
+        if(scores[GetHighestScore(board, moves, scores, values)] == 0){
+            Random random = new Random();
+            int random_move = random.Next(0, moves.Length);
+
+            return moves[random_move];
+        }else{
+        
+            return moves[GetHighestScore(board, moves, scores, values)];
+        }
+    
+
 
     }
 }
