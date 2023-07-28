f683bcf850971a21bc837212d1434be0f5cd5eb5
AssaahhDudeImChad <seb@thelazybasset.com>
2023-07-28 18:16:15 +1200

    Update MyBot.cs

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -8,63 +8,78 @@ public class MyBot : IChessBot
 {   
  
     //null, pawn, knight, bishop, rook, queen, king
-    public Move Think(Board board, Timer timer){   
-        
-        bool color = board.IsWhiteToMove;
+    public Move Think(Board board, Timer timer){
+        bool me_white = true;
+        bool they_white = false;
+        if(board.IsWhiteToMove){
+            me_white = true;
+            they_white = false;
+        }else{
+            they_white = true;
+            me_white = false;
+        }
         Move[] moves = board.GetLegalMoves();
         int[] scores = new int[moves.Length];
         int[] values = {30, 50, 70, 60, 90, 100};
+        int move_weight(Board board, int[] values, Move move, bool they_white){
+            //args = the board, the values, if the opponent is white, 
+            //and who needs to be counted, T for us, F for opponent
+            int countthey = 0;
+            int countme = 0;
 
-        int move_weight(Board board, int[] values, Move move){
-            int count = 0;
             PieceList[] pieces = board.GetAllPieceLists();
-            int countup(int[] values){
+            int countup(int[] values, bool they_white){
+                int white_count = 0;
+                int black_count = 0;
                 for(int i=0; i<6; i++){
-                    count +=(values[i]*pieces[i].Count);     
+                    white_count +=(values[i]*pieces[i].Count);     
                 }
-                for(int x=0; x<6; x++){
-                    count -=(values[x]*pieces[x+6].Count);
-                    
-                }
-                return count;
-                
-            }
             
-            int before = countup(values);
+                for(int x=0; x<6; x++){
+                    black_count +=(values[x]*pieces[x+6].Count);   
+                }
+            
+            if(they_white){
+                countme = black_count;
+                countthey = white_count;
+            }
+            return(countthey);
+            }
+            int before = countup(values, they_white);
             board.MakeMove(move);
-            int after = countup(values);
+            int after = countup(values, they_white);
             board.UndoMove(move);
             //the weight modifier from captures
             int capture_weight = (before-after);
+            Console.WriteLine(capture_weight);
             //getting the weight modifier from checks
-
+     
             board.MakeMove(move);
             bool ischeck = board.IsInCheck();
             bool ismate = board.IsInCheckmate();
             board.UndoMove(move);
             int check_weight = ((Convert.ToInt32(ischeck)+Convert.ToInt32(ismate))*100);
-            Console.WriteLine((check_weight+capture_weight));
             return(check_weight+capture_weight);
             
         }
-        int GetHighestScore(Board board, Move[] moves, int[] scores, int[] values){
+        int GetHighestScore(Board board, Move[] moves, int[] scores, int[] values, bool they_white){
            
             for(int i = 0; i++ < moves.Length-1;){
-                scores[i] += move_weight(board, values, moves[i]);
+                scores[i] += move_weight(board, values, moves[i], they_white);
             }
             int highest_score = scores.Max();
             int highest_index = Array.IndexOf(scores, highest_score);
             return(highest_index);
         }
-        
-        if(scores[GetHighestScore(board, moves, scores, values)] == 0){
+        int index = GetHighestScore(board, moves, scores, values, they_white);
+        if(scores[index] ==0){
             Random random = new Random();
             int random_move = random.Next(0, moves.Length);
 
             return moves[random_move];
         }else{
         
-            return moves[GetHighestScore(board, moves, scores, values)];
+            return moves[GetHighestScore(board, moves, scores, values, they_white)];
         }
     
 
