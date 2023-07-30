b7cfbd7f006374ed0cdf8f727cf7f6673a61dfd2
AssaahhDudeImChad <seb@thelazybasset.com>
2023-07-31 09:16:04 +1200

    not working right now
    Just asafety commiut

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -9,8 +9,8 @@ public class MyBot : IChessBot
  
     //null, pawn, knight, bishop, rook, queen, king
     public Move Think(Board board, Timer timer){
-        bool me_white = true;
-        bool they_white = false;
+        bool me_white;
+        bool they_white;
         if(board.IsWhiteToMove){
             me_white = true;
             they_white = false;
@@ -21,7 +21,7 @@ public class MyBot : IChessBot
         Move[] moves = board.GetLegalMoves();
         int[] scores = new int[moves.Length];
         int[] values = {30, 50, 70, 60, 90, 100};
-        int move_weight(Board board, int[] values, Move[] moves, bool they_white){
+        int move_weight(Board board, int[] values, Move[] moves, bool they_white, Move move, bool single){
             //args = the board, the values, if the opponent is white, 
             //and who needs to be counted, T for us, F for opponent
             int countthey = 0;
@@ -48,20 +48,47 @@ public class MyBot : IChessBot
 
 
             int before = countup(values, they_white);
-            for(int i=0; i++<moves.Length;){
-                board.MakeMove(moves[i]);
+            int after;
+            bool ischeck;
+            bool ismate;
+            if(single){
+                board.MakeMove(move);
+                after = countup(values, they_white);
+                ischeck = board.IsInCheck();
+                ismate = board.IsInCheckmate();
+                board.UndoMove(move);
+            }else{
+                for(int i=0; i++<moves.Length-1;){
+                    board.MakeMove(moves[i]);
+
+                }
+                after = countup(values, they_white);
+                ischeck = board.IsInCheck();
+                ismate = board.IsInCheckmate();
+                for(int i=moves.Length; i>0;){
+                    board.UndoMove(moves[i]);
+
+                }
             }
-            int after = countup(values, they_white);
-            bool ischeck = board.IsInCheck();
-            bool ismate = board.IsInCheckmate();
+            
+
             //the weight modifier from captures
             int capture_weight = (before-after);
-            Console.WriteLine(capture_weight);
             //getting the weight modifier from checks
             int check_weight = ((Convert.ToInt32(ischeck)+Convert.ToInt32(ismate))*100);
-            int[]
             return(check_weight+capture_weight);
             
+        }
+
+        int GetHighestScore(Board board){
+            Move[] moves = board.GetLegalMoves();
+            int[] scores = new int[moves.Length];
+            for(int i=0; i++<moves.Length-1;){
+                scores[i] = move_weight(board, values, moves, they_white, moves[i], true);
+
+            }
+            int index_of_highest = Array.IndexOf(scores, scores.Max());
+            return index_of_highest;
         }
 
         Move search_for_moves(Board board, bool they_white, int depth){
@@ -69,19 +96,22 @@ public class MyBot : IChessBot
             Move[] moves = board.GetLegalMoves();
             int [] weights = new int[moves.Length];
             for(int i=0; i< moves.Length;){
+                Console.WriteLine("move number " + i);
                 Move[] done_moves =new Move[depth];
                 for(int j=0; j++ <depth;){
-                    board.MakeMove(moves[move_weight(board, moves)]);
-                    done_moves[j] = moves[GetHighestScore(board, moves)];
+                    Console.WriteLine("looking in depth " + j);
+                    board.MakeMove(moves[GetHighestScore(board)]);
+                    done_moves[j] = moves[GetHighestScore(board)];
                     
                 }
 
-                weights[i] = move_weight(board, values, done_moves, they_white);
+                weights[i] = move_weight(board, values, done_moves, they_white, done_moves[0], false);
                 for(int k=depth; k-- >depth;){
                     board.UndoMove(done_moves[k]);
                 }  
             }
             int highest_score = weights.Max();
+            Console.WriteLine("highest score from this moveset: "+highest_score);
             int highest_index = Array.IndexOf(weights, highest_score);
             
             if(weights[highest_index] < 0){
@@ -93,7 +123,7 @@ public class MyBot : IChessBot
                 return moves[highest_index];
             }
         }
-        return(search_for_moves(board, they_white, 3));
+        return(search_for_moves(board, they_white, 4));
         }
 
 
