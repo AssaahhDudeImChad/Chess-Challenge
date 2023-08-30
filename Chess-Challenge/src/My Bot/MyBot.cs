55cfedd038640f30f07986c689c488bc4d0e1adc
AssaahhDudeImChad <seb@thelazybasset.com>
2023-08-30 12:01:48 +1200

    Reset, and started again
    I kept getting lost in the code, so I'm starting aain with a more clear path

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -7,93 +7,51 @@ using System.Linq;
 public class MyBot : IChessBot
 {   
  
-    //null, pawn, knight, bishop, rook, queen, king
-    public Move Think(Board board, Timer timer){   
-        
-        bool color = board.IsWhiteToMove;
-        Move[] moves = board.GetLegalMoves();
-        int[] scores = new int[moves.Length];
-        int[] values = {30, 50, 70, 60, 90, 100};
+    public Move Think(Board board, Timer timer){  
+        bool AI_is_white = board.IsWhiteToMove;
+        //Fucntion to get the board weight in the favour/dis Favour of the AI 
+        int get_board_weight(){
 
-        int move_weight(Board board, int[] values, Move move){
-            int count = 0;
+            //loop throguh the piece list and get white and black values
+
             PieceList[] pieces = board.GetAllPieceLists();
-            int countup(int[] values){
-                for(int i=0; i<6; i++){
-                    count +=(values[i]*pieces[i].Count);     
-                }
-                for(int x=0; x<6; x++){
-                    count -=(values[x]*pieces[x+6].Count);
-                    
-                }
-                return count;
-                
+            int white_peices = 0;
+            int black_peices = 0;
+            //Pawns, Knights, Bishops,Rooks, Queen, King
+            int[] weights = {1, 3, 3, 5, 9, 100};
+            int board_weight = 0;
+            //peices are listed white then black so loops through the whites then the black
+            for(int i = 0; i < 6; i++){
+                white_peices += (pieces[i].Count * weights[i]);
             }
-            
-            int before = countup(values);
-            Console.WriteLine("checking move: "+ move);
-            board.MakeMove(move);
-            int after = countup(values);
-            board.UndoMove(move);
-            //the weight modifier from captures
-            int capture_weight = before-after;
-            //getting the weight modifier from checks
+            for(int y = 0; y <6; y++){
+                black_peices += (pieces[y+6].Count * weights[y]);
+            }
+            if(AI_is_white){
+                board_weight = white_peices - black_peices;
+            }else{
+                board_weight = black_peices - white_peices;
+            }
 
-            board.MakeMove(move);
-            bool ischeck = board.IsInCheck();
-            bool ismate = board.IsInCheckmate();
-            board.UndoMove(move);
-            int check_weight = ((Convert.ToInt32(ischeck)+Convert.ToInt32(ismate))*100);
-            return check_weight+capture_weight;
-            
+            return board_weight;
+        
         }
-        int GetHighestScore(Board board, int[] scores, int[] values){
-            Console.WriteLine("GetHighest Score");
-            Move[] moves = board.GetLegalMoves();
-            for(int i = 0; i++ < moves.Length-1;){
-                scores[i] += move_weight(board, values, moves[i]);
-            }
-            int highest_score = scores.Max();
-            int highest_index = Array.IndexOf(scores, highest_score);
-            return(highest_index);
-        }
+        Console.WriteLine(get_board_weight());
+
+        //Func to get the current best move, basically looks through the moves and runs
+        //board weight for each move and returns the best move
 
 
-        int[] Search_moves(int depth){
-            Console.WriteLine("Running search_moves");
-            Move[] done_moves = new Move[depth];
-            int[] scores = new int[board.GetLegalMoves().Length];
-
-            for(int i = 0;i < depth;i++){
-
-                Move[] moves = board.GetLegalMoves();
-                int index = GetHighestScore(board,scores, values);
-                Console.WriteLine("In depth" + i +"looking through move" + moves[index]);
-                board.MakeMove(moves[index]);
-
-                done_moves[i] = moves[index];
-                scores[i] += move_weight(board, values, moves[index]);
-                
-            }
-            for(int j = depth; j >  0;j--){
-                board.UndoMove(done_moves[j]);
-            }
-            return(scores);
-
-        }
-        Search_moves(3);
-        int highest_score = scores.Max();
-        if(highest_score ==0){
-            Random random = new Random();
-            int random_move = random.Next(0, moves.Length);
-
-            return moves[random_move];
-        }else{
-            int index_of_highest = Array.IndexOf(scores, highest_score);
-            return moves[index_of_highest]; 
+        //The main loop, gets the best move, makes it, starts again for depth x
+        //Then runs a final board countup function for that branch, and adds 
+        // that to the array 
     
+        // call the main loop
 
 
+
+
+        Move[] moves = board.GetLegalMoves();
+        return moves[0];
     }
-}
 }
