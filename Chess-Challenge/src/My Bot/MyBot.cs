d5f3e66e6113dd5535837072a908a89e072f0d58
AssaahhDudeImChad <85378098+AssaahhDudeImChad@users.noreply.github.com>
2023-09-01 15:25:27 +1200

    Progress ish

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -7,178 +7,163 @@ using System.Linq;
 public class MyBot : IChessBot
 {   
  
-    //null, pawn, knight, bishop, rook, queen, king
-<<<<<<< Updated upstream
-    public Move Think(Board board, Timer timer){   
-        
-        bool color = board.IsWhiteToMove;
-=======
-    public Move Think(Board board, Timer timer){
-        bool me_white;
-        bool they_white;
-        //gets if it is white's turn to move
-        if(board.IsWhiteToMove){
-            me_white = true;
-            they_white = false;
-        }else{
-            they_white = true;
-            me_white = false;
-        }
->>>>>>> Stashed changes
+    public Move Think(Board board, Timer timer){  
+
+        bool AI_is_white = board.IsWhiteToMove;
+        Random random = new Random();
         Move[] moves = board.GetLegalMoves();
-        int[] scores = new int[moves.Length];
-        int[] values = {0, 30, 50, 70, 60, 90, 100};
+        //Fucntion to get the board weight in the favour/dis Favour of the AI 
+        int get_board_weight(bool white){
 
-        int countup(Board board, int[] values){
-            int count = 0;
+            //loop throguh the piece list and get white and black values
+
             PieceList[] pieces = board.GetAllPieceLists();
-            for(int i=0; i<5; i++){
-                count +=(values[i]*pieces[i].Count);
+            int white_peices = 0;
+            int black_peices = 0;
+            //Pawns, Knights, Bishops,Rooks, Queen, King
+            int[] weights = {1, 3, 3, 5, 9, 100};
+            int board_weight = 0;
+            //peices are listed white then black so loops through the whites then the black
+            for(int i = 0; i < 6; i++){
+                white_peices += (pieces[i].Count * weights[i]);
             }
-            return(count);
-            
-        }
-        static int pieceweight(Piece piece){
-         
-
-<<<<<<< Updated upstream
-            if(piece.IsPawn){
-                return(30);
-            }else if(piece.IsKnight){
-                return(50);
-            }else if(piece.IsRook){
-                return(60);
-            }else if(piece.IsBishop){
-                return(70);
-            }else if(piece.IsQueen){
-                return(90);
-            }else if(piece.IsKing){
-                return(100);
+            for(int y = 0; y <6; y++){
+                black_peices += (pieces[y+6].Count * weights[y]);
+            }
+            if(white){
+                board_weight = black_peices-white_peices;
             }else{
-                return(0);
-            }
-        }
-        static int ischeck_or_mate(Board board, Move move){
-            board.MakeMove(move);
-            bool ischeck = board.IsInCheck();
-            bool ismate = board.IsInCheckmate();
-            board.UndoMove(move);
-            if(ischeck){
-                return(1);
-            }else if(ismate){
-                return(2);
-            }else{
-                return(0);
+                board_weight = white_peices-black_peices;
             }
 
-        }
-        Console.WriteLine(countup(board, values));
+            return board_weight;
         
-        int GetHighestScore(Board board, Move[] moves, int[] scores){
-           
-            for(int i = 0; i++ < moves.Length-1;){
-         
-                bool capture = moves[i].IsCapture;
-                if(capture == true){
-                    
-                    var capture_type = moves[i].TargetSquare;
-                    Piece target = board.GetPiece(capture_type);
-                    scores[i] += pieceweight(target);
+        }
+
+        //Func to get the current best move, basically looks through the moves and runs
+        //board weight for each move and returns the best move
+        Move get_best_move(bool white){
+            Move[] legal_moves = board.GetLegalMoves();
+            
+            int[] move_weights = new int[legal_moves.Length];
+            for(int i = 0; i < legal_moves.Length; i++){
+                //get the current weight, make the move, get the weight after, and compare
+                int before_weight = get_board_weight(white);
+                board.MakeMove(legal_moves[i]);
+                int after_weight = get_board_weight(white);
+                if(board.IsInCheck()){
+                    //see if black king is in check or mate
+                    after_weight += 8;
                 }
-                int check_mate = ischeck_or_mate(board, moves[i]);
-                if(check_mate == 1){
-                    scores[i] += 250;
-                }else if(check_mate == 2){
-                    scores[i] += 1000000000;
+                board.UndoMove(legal_moves[i]);
+
+                int move_weight = (after_weight-before_weight);
+                move_weights[i] = move_weight;
+            }
+            int max = move_weights.Max();
+            int best_move_index = Array.IndexOf(move_weights, max);
+            int check_random(){
+                int num = random.Next(0, legal_moves.Length);
+                if(max == 0){
+
+                    Move move = legal_moves[num];
+                    int move_type = (int) move.MovePieceType;
+                    if(move_type == 1){
+                        Console.WriteLine("returning pawn move");
+                        return num;
+
+                    }else{
+                        Console.WriteLine("Reutrning");
+                        check_random();
+                        return 0;
+
+                    }
+                }else{
+                    return best_move_index;
                 }
-=======
+                }
+            best_move_index = check_random();
+            return legal_moves[best_move_index];
+        }   
+        //main loop, for EACH move currentlt available, make it, then look at the best
+        //make it and continue for depth x
+        //Then runs a final board countup function for that branch, and adds 
+        // that to the array 
 
-            int before = countup(values, they_white);
-            int after;
-            bool ischeck;
-            bool ismate;
-        
-            board.MakeMove(move);
-            after = countup(values, they_white);
-            ischeck = board.IsInCheck();
-            ismate = board.IsInCheckmate();
-            board.UndoMove(move);
-        
-            
+        //for loop:
+        //get best move, and make it
+        //Get best move for white/ black relative to what the AI is and who's turn it is
+        //Do this for x times, and do a final board countup, append that to the main list
+        int depth = 5;
+        int [] final_move_weights = new int[board.GetLegalMoves().Length];
 
-            //the weight modifier from captures
-            int capture_weight = (before-after);
-            //getting the weight modifier from checks
-            int check_weight = ((Convert.ToInt32(ischeck)+Convert.ToInt32(ismate))*100);
-            return(check_weight+capture_weight);
-            
-        }
+        for(int x = 0; x < board.GetLegalMoves().Length; x++){
+            //board.MakeMove(board.GetLegalMoves()[x]);
+            //Console.WriteLine("Starting move: " + board.GetLegalMoves()[x]);
+            Move[] done_moves = new Move[depth];
+            Move picked_move = board.GetLegalMoves()[0];
+            done_moves[0] = board.GetLegalMoves()[x];
+            for(int i = 1; i < depth; i++){
+                if(board.IsInCheck()){
+                    final_move_weights[x] += 101;
+                    break;
+                }
+                //if it's odd, look at OUR best move, if othersie look at THEIR best move
+                if(i % 2 == 0){
+                    //Check that if someone is in check, Stop the loop and add the max value to the weights
 
-        int GetHighestScore(Board board){
-            Move[] moves = board.GetLegalMoves();
-            int[] scores = new int[moves.Length];
-            for(int i=0; i<moves.Length;i++){
-                scores[i] = move_weight(board, values, moves, they_white, moves[i], true);
-                Console.WriteLine("Score for move "+ i+  ": "+scores[i]);
+                    //even
+                    //find THEIR best move and make it
+                    if(AI_is_white){
+                        //find best move white=false
+                       picked_move = get_best_move(false);
 
+                    }else{
+                        //find best move white=true
+                       picked_move =get_best_move(true);
+                    }
+                    }else{
+                    //odd
+                    //find OUR best move and make it
+                    if(AI_is_white){
+                        //find best move white=true
+                       picked_move = get_best_move(true);
+                    }else{
+                        //fine best move whtie=false
+                       picked_move = get_best_move(false);
+                    }
+                        
+                    }
+                
+                //make that move
+                board.MakeMove(picked_move);
+                done_moves[i]  = picked_move;
+                //re-start the loop and at the END run a countup for the loop 
+            }
+            //countup the board values
+            //Something is wrong here.... they are blank ints...
+            final_move_weights[x] += get_board_weight(AI_is_white);
+            for(int i = done_moves.Length-1; i > 0; i--){
+                board.UndoMove(done_moves[i]);
             }
 
-            int index_of_highest = Array.IndexOf(scores, scores.Max());
-            return index_of_highest;
+        } 
+        //TODO;
+        //ITs not undoing the moves in the right order
+        //this means it registers the WRONG current avaialbel mvoes, it's picking the best ones but 
+        //it isnt executing the right ones if that make sense
+        //find highest score in array
+        int index_of_best_move = Array.IndexOf(final_move_weights, final_move_weights.Max());
+        // call the main loop
+        Move move = get_best_move(true);
+        Move[] legal_moves = board.GetLegalMoves();
+        //Console.WriteLine("moves availabel: "+legal_moves.Length);
+        //Console.WriteLine("index of best move: " +(index_of_best_move-1));
+        if(legal_moves[index_of_best_move].IsNull == true){
+            return(legal_moves[random.Next(0, legal_moves.Length)]);
         }
-
-        Move search_for_moves(Board board, bool they_white, int depth){
-            
-            Move[] moves = board.GetLegalMoves();
-            int name = 1;
-            int [] weights = new int[moves.Length];
-            Console.WriteLine(moves.Length);
-            for(int i=0; i< moves.Length; i++){
-                Console.WriteLine("Lookinin move"+ i +" of"+ weights.Length);
-                Move[] done_moves =new Move[depth];
-                //for(int j=0; j<depth; j++){
-                    
-                //}
-
-                weights[i] = move_weight(board, values, done_moves, they_white, done_moves[0], false);
-                Console.WriteLine("move weight = " + weights[i]);
-            }
-            int highest_score = weights.Max();
-            
-            Console.WriteLine("highest score from this moveset: "+highest_score);
-            int highest_index = Array.IndexOf(weights, highest_score);
-
-            if(weights[highest_index] < 1){
-                Console.WriteLine("using random");
-                Random random = new Random();
-                int random_move = random.Next(0, moves.Length);
-                return moves[random_move];
-            }
-            else{
-                Console.WriteLine("Using regularol");
-                return moves[highest_index];
->>>>>>> Stashed changes
-            }
-            int highest_score = scores.Max();
-            int highest_index = Array.IndexOf(scores, highest_score);
-            return(highest_index);
-        }
-<<<<<<< Updated upstream
-       
-        int index = GetHighestScore(board, moves, scores);
-        
-        if(scores[index] == 0){
-            Random random = new Random();
-            int random_move = random.Next(0, moves.Length);
-
-            return moves[random_move];
-        }else{
-         
-            return moves[index];
-=======
-        return(search_for_moves(board, they_white, 3));
->>>>>>> Stashed changes
-        }
-
+        return legal_moves[index_of_best_move];
     }
 }
+
+//test for github
